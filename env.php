f2d4e3371cf8cb444496d6620cb63bfabe3f568c
Jedial <alexandre.brunier.96@gmail.com>
2017-06-13 13:38:20 +0000

    J'ai modifier le header et le css bisou

--- env.php
+++ env.php
@@ -4,6 +4,6 @@ return [
     "dbname" => "php_projet-m2105",
     "dbhost" => "localhost",
     "dbuser" => "root",
-    "dbpass" => "",
+    "dbpass" => "JAir96jedi09?",
     "charset" => "utf8"
 ];
