41300f94b6b880140d05fd9038269052a01191b2
Daethe <marc.proux76@gmail.com>
2017-06-20 08:59:54 +0200

    Merged

--- Views/static/header.php
+++ Views/static/header.php
@@ -6,5 +6,26 @@
  * Time: 13:57
  */
 ?>
-
-<h1>Je suis un header trop cool</h1>
+<div class="top-bar" style="margin-bottom:20px">
+	<div class="top-bar-left">
+		<ul class="dropdown menu" data-dropdown-menu>
+			<li class="menu-text">Site Title</li>
+			<li>
+				<a href="#">One</a>
+				<ul class="menu vertical">
+					<li><a href="#">One</a></li>
+					<li><a href="#">Two</a></li>
+					<li><a href="#">Three</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Two</a></li>
+			<li><a href="#">Three</a></li>
+		</ul>
+	</div>
+	<div class="top-bar-right">
+		<ul class="menu">
+			<li><a href="?p=login">Connexion</a></li>
+			<li><a href="?p=register">S'enregistrer</a></li>
+		</ul>
+	</div>
+</div>
